681252fb0f68fddb7ec42d58d7e98c17af11b54e
Ritik Pratap Singh Patel <168630702+patelritiq@users.noreply.github.com>
2026-01-20 15:58:40 +0530

    Delete my-first-web-application directory

--- my-first-web-application/StudentApi/Models/State.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace StudentApi.Models
-{
-    public class State
-    {
-        public int StateId { get; set; }
-        public string StateName { get; set; } = string.Empty;
-    }
-
-}
--- my-first-web-application/StudentUi/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { StudentComponent } from './components/student/student';
-
-export const routes: Routes = [
-  { path: '', redirectTo: '/students', pathMatch: 'full' },  
-  { path: 'students', component: StudentComponent },                      
-  { path: '**', redirectTo: '/students' }                    
-];
